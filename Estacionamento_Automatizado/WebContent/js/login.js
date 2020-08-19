61bd672b2b560e445b094847cce4dc3192018c73
Samoel <samoellaureano@gmail.com>
2020-08-18 21:58:42 -0300

    update front 18/08/2020

--- Estacionamento_Automatizado/WebContent/js/login.js
+++ Estacionamento_Automatizado/WebContent/js/login.js
@@ -2,7 +2,7 @@ assistencia = new Object();
 usuario = new Object();
 
 $(document).ready(function(){
-    $('#inputCPF').mask('000.000.000-00');
+    $('#usuario').mask('000.000.000-00');
 });
 
 assistencia.login = function(){
