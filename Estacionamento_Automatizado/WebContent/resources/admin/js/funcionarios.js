730fb4caacc02e1b67831825e1b74f1de3ac997f
Samoel <samoellaureano@gmail.com>
2020-08-26 21:48:27 -0300

    Finalizado o front do funcionario

--- Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
+++ Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
     $("#menu").load("menu.html");
 });
+
+$('#carrega-listaFuncionario').click(function(e){
+    /*https://tableless.com.br/conteudo-sob-demanda-com-jquery/ */
+  });
