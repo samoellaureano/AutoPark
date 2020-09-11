49b2b9e422d97adffa593357764288c9ffb05e4a
Samoel <samoellaureano@gmail.com>
2020-09-10 22:05:55 -0300

    Ajuste do Ajax

--- Estacionamento_Automatizado/WebContent/js/ajax.js
+++ Estacionamento_Automatizado/WebContent/js/ajax.js
@@ -1,12 +1,5 @@
-//Criando o objeto genérico SENAI
-IT = new Object();
-//Criando o sub-objeto ajax do objeto SENAI.
-IT.ajax = new Object();
-
-/*
- * Processa o pedido, solicitação HTTP Ajax a ser recebido pelo Rest.
- */
-
+autoPark = new Object();
+autoPark.ajax = new Object();
 function ajaxRequestDefault(){
 	var def = {
 			url: null,
@@ -20,18 +13,6 @@ function ajaxRequestDefault(){
 	};
 	return def;
 }
-/* Verifica o estado do objeto cfg recebido, ou seja, se o identificador cfg
- * trata-se realmente de uma variavel de objeto contendo suas respectivas
- * propriedades com valores, se isto se confirmar retorna o objeto cfg.
- * Notem a chamada ao método stringfy() que, por sua vez, converte o conteúdo
- * dos dados encontrados em cfg. data em JSON para que posteriormente possam
- * ser repassados pelo Ajax a partir de um pedido HTTP.
- * Notem tambem a chamada à função isObject(cfg.data) que repassa como
- * parâmetro cfg.data
- * para saber se os dados apontados por cfg tratam-se de um array de dados,
- * ou se é um objeto semples, criado como {} ou new Object e por ultimo
- * verifica se trata-se de uma função objeto Javascript.
- */
 function verifyObjectData(cfg){
 	if(cfg.data){
 		if(isObject(cfg.data)){
@@ -40,48 +21,18 @@ function verifyObjectData(cfg){
 	}
 	return cfg;
 }
-
-/*A função abaixo verifica os dados apontados por cfg tratam-se de um array
- * de dados, ou se é um objeto simples, criado como {} ou new Object e por
- * ultimo verifica se trata-se de uma função objeto Javascript. Retorna o tipo
- * estrutura passada pelo objeto cfg.
- */
 function isObject(o){
 	return $.isArray(o) | $.isPlainObject(o) | $.isFunction(o);
 };
 
-IT.ajax.post = function(cfg){
-	/*
-	 * Inicia o Ajax e processa um pedido de Ajax, a partir de
-	 * ajaxRequestDefault().
-	 * esta inicialização e pedido são repassadas para o objeto def que,
-	 * por sua vez, passará a ser um Ajax solicitante de uso geral.
-	 */
+autoPark.ajax.post = function(cfg){
 	var def = new ajaxRequestDefault();
-
-	/*
-	 * chama a função verifyObjectData() que, por sua vez, verifica o 
-	 * estado do objeto cfg recebido, ou seja, se o identificador cfg
-	 * trata-se ralmente de uma variavel de objeto contendo suas
-	 * respectivas propriedades com valores, se isto se confirmar retorna o
-	 * objeto cfg e o armazena tambem em outro 
-	 * objeto cfg que será fundido no objeto config para que uma solicitação
-	 * pedido HTTP, Ajax seja enviada para um recurso Rest
-	 */
 	cfg = verifyObjectData(cfg);
-	/*
-	 * Fundindo os objetos def e cfg e armazenando seus respectivos
-	 * valores em config.
-	 */
 	var config = $.extend(def,cfg);
-
-	/*
-	 * Realizando um HTTP pedido ajax. Em o parâmetro config segue o pedido Ajax.
-	 */
 	$.ajax(config);
 };
 
-IT.ajax.get = function(cfg){
+autoPark.ajax.get = function(cfg){
 	var def = new ajaxRequestDefault();
 	cfg.type = "GET";
 	cfg = verifyObjectData(cfg);
--- Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
+++ Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
@@ -1,8 +1,46 @@
+var funcionario = new Object();
 $(document).ready(function(){
     $("#menu").load("menu.html");
+
+    $('#cadFuncionario').click(function(e){
+        var arrayCadFun = [];
+        var $inputs = $('#formCadFuncionario :input');
+        // percorre os inputs
+        $inputs.each(function() {
+            arrayCadFun.push($(this).val());
+        });
+        funcionario.name = arrayCadFun[0];
+        funcionario.celular = arrayCadFun[1];
+        funcionario.email = arrayCadFun[2];
+        funcionario.empresa = arrayCadFun[3];
+
+        var cfg = {
+            url: "../rest/funcionarioRest/addFuncionario",
+            data: JSON.stringify(funcionario),
+            success: function (succJson) {
+                if (succJson == 1) {
+                    resp = ("Funcionário cadastrado com sucesso!");
+                    exibirMessagem(resp, 1);
+                } else if(succJson == 2){
+                    resp = ("O Funcionário ja existe!");
+                    exibirMessagem(resp, 2);
+                }else{
+                    resp = ("Erro ao cadastrar um novo funcionário!");
+                    exibirMessagem(resp, 2);
+                }
+                
+                // funcionario.buscar();
+            },
+            error: function (errJson) {
+                resp = ("Erro ao cadastrar um novo funcionário!");
+                exibirMessagem(resp, 2);
+            }
+        };
+        autoPark.ajax.post(cfg);
+    });
+
+    $('#carrega-listaFuncionario').click(function(e){
+        /*https://tableless.com.br/conteudo-sob-demanda-com-jquery/ */
+    });
 });
 
-
-$('#carrega-listaFuncionario').click(function(e){
-    /*https://tableless.com.br/conteudo-sob-demanda-com-jquery/ */
-  });
