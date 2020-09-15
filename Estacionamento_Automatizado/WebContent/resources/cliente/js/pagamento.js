25e2b18acf5015c05db42c2a91fbbda65c22afde
felipe <symsmage94@gmail.com>
2020-09-14 23:40:44 -0300

    Js cadastro de cliente, veiculo e inicio da construção dasdas views

--- Estacionamento_Automatizado/WebContent/resources/cliente/js/pagamento.js
+++ Estacionamento_Automatizado/WebContent/resources/cliente/js/pagamento.js
@@ -1,42 +1,66 @@
-var veiculo = new Object();
+var pagamento = new Object();
 $(document).ready(function(){
     $("#menu").load("menu.html");
 
     $('#cadPagamento').click(function(e){
-        var arrayCadFun = [];
-        var $inputs = $('#formCadPagamento :input');
-        // percorre os inputs
-        $inputs.each(function() {
-            arrayCadFun.push($(this).val());
-        });
-        veiculo.numCartao = arrayCadFun[0];
-        veiculo.titular = arrayCadFun[1];
-        veiculo.validade = arrayCadFun[2];
-        veiculo.codVerificacao = arrayCadFun[3];
-
+       
+        pagamento.numCartao = $("#cartao").val()
+        pagamento.titular = $("#NomeTitular").val()
+        pagamento.validade = $("#validade").val()
+        pagamento.codVerificacao = $("#codigoVerificacao").val()
+        
         var cfg = {
-            url: "../rest/veiculosRest/addveiculos",
-            data: JSON.stringify(veiculo),
+            url: "../rest/pagamentosRest/addPagamentos",
+            data: JSON.stringify(pagamento),
             success: function (succJson) {
                 if (succJson == 1) {
-                    resp = ("Veiculos cadastrado com sucesso!");
+                    resp = ("pagamentos cadastrado com sucesso!");
                     exibirMessagem(resp, 1);
                 } else if(succJson == 2){
-                    resp = ("O Veiculo ja existe!");
+                    resp = ("O pagamento ja existe!");
                     exibirMessagem(resp, 2);
                 }else{
-                    resp = ("Erro ao cadastrar um novo Veiculo!");
+                    resp = ("Erro ao cadastrar um novo pagamento!");
                     exibirMessagem(resp, 2);
-                }
-                
-                // funcionario.buscar();
+                }             
             },
             error: function (errJson) {
-                resp = ("Erro ao cadastrar um novo Veiculo!");
+                resp = ("Erro ao cadastrar um novo Pagamento!");
                 exibirMessagem(resp, 2);
             }
         };
         autoPark.ajax.post(cfg);
     });
+
+
+    buscar = function(){    
+       
+        var cfg = {
+            url: "../rest/pagamentosRest/viewPagamentos",
+            data: JSON.stringify(pagamento),
+            success: function (pagamento) {
+               visualizar(pagamento);
+            },  
+            error: function (errJson) {
+                resp = ("Erro ao cadastrar um novo Pagamento!");
+                exibirMessagem(resp, 2);
+            }
+        };
+
+        autoPark.ajax.post(cfg);
+    };// fim do buscar
+
+    visualizar=function(pagamento){
+
+        if( pagamento!=null && pagamento.length>0){
+
+            $("#cartao").val(pagamento.numCartao);
+            $("#NomeTitular").val(pagamento.titular);
+            $("#validade").val(pagamento.validade);
+            $("#codigoVerificacao").val(pagamento.codVerificacao);
+        };
+    };  // fim do visualizar
+
+    buscar();
 });
 
--- Estacionamento_Automatizado/WebContent/resources/cliente/js/veiculos.js
+++ Estacionamento_Automatizado/WebContent/resources/cliente/js/veiculos.js
@@ -3,40 +3,83 @@ $(document).ready(function(){
     $("#menu").load("menu.html");
 
     $('#cadVeiculo').click(function(e){
-        var arrayCadFun = [];
-        var $inputs = $('#formCadVeiculo :input,select');
-        // percorre os inputs
-        $inputs.each(function() {
-            arrayCadFun.push($(this).val());
-        });
-        veiculo.marca = arrayCadFun[0];
-        veiculo.modelo = arrayCadFun[1];
-        veiculo.ano = arrayCadFun[2];
-        veiculo.placa1 = arrayCadFun[3];
+   
+        veiculo.marca = $("#marca").val();
+        veiculo.modelo = $("#modelo").val();
+        veiculo.ano = $("#ano").val();
+        veiculo.placa1 = $("#placa").val();
 
         var cfg = {
             url: "../rest/veiculoRest/addVeiculo",
             data: JSON.stringify(veiculo),
             success: function (succJson) {
                 if (succJson == 1) {
-                    resp = ("Pagamento cadastrado com sucesso!");
+                    resp = ("Veiculo cadastrado com sucesso!");
                     exibirMessagem(resp, 1);
                 } else if(succJson == 2){
-                    resp = ("O Pagamento ja existe!");
+                    resp = ("O Veiculo ja existe!");
                     exibirMessagem(resp, 2);
                 }else{
-                    resp = ("Erro ao cadastrar um novo Pagamento!");
+                    resp = ("Erro ao cadastrar um novo Veiculo!");
                     exibirMessagem(resp, 2);
-                }
-                
-                // funcionario.buscar();
+                }               
             },
             error: function (errJson) {
-                resp = ("Erro ao cadastrar um novo Pagamento!");
+                resp = ("Erro ao cadastrar um novo Veiculo!");
                 exibirMessagem(resp, 2);
             }
         };
         autoPark.ajax.post(cfg);
     });
-});
 
+
+    busca=function(){
+        var cfg = {
+            url: "../rest/veiculoRest/buscaVeiculo",
+            data: JSON.stringify(veiculo),
+            success: function (listaVeiculo) {
+                 visualizar(listaVeiculo);
+            },
+            error: function (errJson) {
+                resp = ("Erro ao cadastrar um novo Veiculo!");
+                exibirMessagem(resp, 2);
+            }
+        };
+        autoPark.ajax.post(cfg);
+
+        visualizar=function(listaVeiculo){
+
+            var html =" <ul class='itemVeiculo'><input type='radio' name='carro' id='car3' hidden>";
+
+            for (var i = 0; i < listaVeiculo.length; i++) {
+               
+                
+            }
+
+
+
+
+        };
+
+
+
+
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+});
