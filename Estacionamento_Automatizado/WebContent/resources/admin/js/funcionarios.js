d043a20ee77044d829e75d6b0368a77e89837e2f
Samoel <samoellaureano@gmail.com>
2020-09-15 22:33:43 -0300

    Finalizado o JS de funcionario

--- Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
+++ Estacionamento_Automatizado/WebContent/resources/admin/js/funcionarios.js
@@ -21,15 +21,78 @@ $(document).ready(function(){
         var funcionariosHTML = "<ul class='itemFuncionario'>";
         if (listaDeFuncionarios != undefined) {
             if (listaDeFuncionarios.length > 0) {
-                for (var i = 0; i < listaDeMarcas.length; i++) {
-                    funcionariosHTML += ("<input type='radio' name='funcionario' id='func"+i+"' hidden>");
+                for (var i = 0; i < listaDeFuncionarios.length; i++) {
+                    if(listaDeFuncionarios.status){
+                        listaDeFuncionarios.status = "checked";
+                    }else{
+                        listaDeFuncionarios.status = "";
+                    }
+
+                    if(listaDeFuncionarios.perfil == 0){
+                        listaDeFuncionarios.perfil = "<option value='0' selected>Administrador</option>"
+                            +"<option value='1'>Funcionario</option>"
+                            +"<option value='2'>Cliente</option></select>";
+                    }else if(listaDeFuncionarios.perfil == 1){
+                        listaDeFuncionarios.perfil = "<option value='0'>Administrador</option>"
+                            +"<option value='1' selected>Funcionario</option>"
+                            +"<option value='2'>Cliente</option></select>";
+                    }else{
+                        listaDeFuncionarios.perfil = "<option value='0'>Administrador</option>"
+                            +"<option value='1'>Funcionario</option>"
+                            +"<option value='2' selected>Cliente</option></select>";
+                    }
+                    funcionariosHTML += "<input type='radio' name='funcionario' id='func"+i+"' hidden>"
+                    +"<label for='func"+i+"'>"+listaDeFuncionarios.nome+"</label>"
+                    +"<li><label for='editar"+i+"'>Editar</label>"
+                    +"<input type='checkbox' name='editar' id='editar"+i+"' hidden>"
+                    +"<div><form><label for='nome'>Nome:</label><input type='text' id='nome"+i+"'"
+                    +"value='"+listaDeFuncionarios.nome+"'>"
+                    +"<label for='celular'>Celular:</label><input type='text' id='celular"+i+"'"
+                    +"value='"+listaDeFuncionarios.celular+"'>"
+                    +"<label for='email'>Email:</label><input type='text' id='email"+i+"'"
+                    +"value='"+listaDeFuncionarios.email+"'>"
+                    +"<label for='perfil'>Perfil:</label>"
+                    +"<select name='perfil' id='perfil"+i+"'>"
+                    + listaDeFuncionarios.perfil
+                    +"<label for='status'>Status:<input type='checkbox' id='status"+i+"' "+ listaDeFuncionarios.status +"></label>"
+                    +"<div><a href=''>Cancelar</a>"
+                    +"<button onclick='editarFuncionario("+i+")'>Confirmar</button></div></form></div></li>"
                 }
+                funcionariosHTML+="</ul><a href='' id='carrega-listaFuncionario'>Ver Mais</a>";
             } else {
                 funcionariosHTML += "<li style='text-align: center'>Nenhum registro encontrado</li>";
             }
-            $("#listaFuncionariosHTML").html(funcionariosHTML + "</ul>");
+            $("#listaFuncionariosHTML").append(funcionariosHTML);
         }
     }
+
+    editarFuncionario = function(id){
+        funcionario.nome = $("#nome"+id).val();
+        funcionario.celular = $("#celular"+id).val();
+        funcionario.email = $("#email"+id).val();
+        funcionario.perfil = $("#perfil"+id).val();
+        funcionario.status = $("#status"+id).val();
+
+        var cfg = {
+            url: "../rest/funcionarioRest/editarFuncionario",
+            data: JSON.stringify(funcionario),
+            success: function (succJson) {
+                if (succJson == 1) {
+                    resp = ("Funcionário editado com sucesso!");
+                    exibirMessagem(resp, 1);
+                }else{
+                    resp = ("Erro ao editar o funcionário!");
+                    exibirMessagem(resp, 2);
+                }
+                buscar();
+            },
+            error: function (errJson) {
+                resp = ("Erro ao editar o funcionário!");
+                exibirMessagem(resp, 2);
+            }
+        };
+        autoPark.ajax.post(cfg);
+    };
 
     $('#cadFuncionario').click(function(e){
         funcionario.nome = $("#nome").val();
