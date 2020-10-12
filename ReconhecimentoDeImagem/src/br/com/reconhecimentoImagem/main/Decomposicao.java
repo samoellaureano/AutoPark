c6af4c6a233f2d5e4e94c4d1bc4822f81310c42a
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-10-12 16:11:12 -0300

    ja realiza a conversão do arquivo gerado pelo tesseract em uma string nao formatada porém o tesseract não inicia pelo processBuilder do eclipse

--- ReconhecimentoDeImagem/src/br/com/reconhecimentoImagem/main/Decomposicao.java
+++ ReconhecimentoDeImagem/src/br/com/reconhecimentoImagem/main/Decomposicao.java
@@ -2,6 +2,7 @@ package br.com.reconhecimentoImagem.main;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -13,115 +14,95 @@ import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
 public class Decomposicao {
-//tesseract.exe C:\Users\Felipe\Documents\GitHub\TCC\ReconhecimentoDeImagem\placa2.png C:\Users\Felipe\Documents\GitHub\TCC\ReconhecimentoDeImagem\out  --dpi 200
+	//tesseract.exe C:\Users\Felipe\Documents\GitHub\TCC\ReconhecimentoDeImagem\placa2.png C:\Users\Felipe\Documents\GitHub\TCC\ReconhecimentoDeImagem\out  --dpi 200
 	private static int eixoX=0;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 
-		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);		     
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 
-		String localImg="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa2.jpg";
+		String localImg="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa1.jpg";
+		String localImg1="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa2.jpg";
+		String localImg2="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa4.jpg";
 
-
-		/*A classe Mat representa um arranjo numérico denso e único canal ou multicanal n-dimensional. 
-		 * Pode ser usado para armazenar vetores e matrizes reais ou complexos, imagens em escala de cinza ou em cores. 
-		 */
-
-		Mat img2 = Imgcodecs.imread(localImg);	
-		mostraImg(img2,"Placa_normal"); 
-		Imgproc.cvtColor(img2, img2, Imgproc.COLOR_BGR2GRAY);
-		Imgproc.equalizeHist(img2, img2);
+		Mat img = Imgcodecs.imread(localImg1);
 		
+		Imgproc.cvtColor(img, img, Imgproc.COLOR_BGR2GRAY);		 
+		Imgproc.medianBlur(img, img, 3);
+		Imgproc.GaussianBlur(img, img, new Size(5,5),5);
+		
+		Imgproc.medianBlur(img, img, 5);
+	 // Imgproc.equalizeHist(img2, img2);
+		Imgproc.adaptiveThreshold(img, img, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,11,11);	
+		
+	 // Imgproc.resize(img2,img2,new Size(300,300));
+		Imgcodecs.imwrite("placa2.png",img );		
+		mostraImg(img, "Imagem");
+				
+		Process converter=null;
+		try {
+			converter = new ProcessBuilder("tesseract.exe "," C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\placa2.png  C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\out --dpi 900").start();
+
+			String texto = lerArquivo("C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\out.txt");
+
+			System.out.println("texto: "+texto);
+			//	aaa.destroy();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+
+		finally {
+			converter.destroy();
+		}
 
 		/* O filtro Gaussiano geralmente é usado como um filtro de passa-baixa por deixar passar 
 		 * as baixas frequências, mas elimina os valores relacionados às altas frequência.  
 		 * Neste filtro, assim como em outros, a suavização da imagem é realizada através da 
 		 * substituição de cada pixel pela média ponderada dos pixels vizinhos. 
-		 */
+		 * 
+		 * Filtro da Mediana: Eficiente para o propósito de filtrar imagens, pois ele reduz a quantidade de variação de 
+		 * intensidade entre um pixel e seus vizinhos, eliminando ruídos.
+		 */		
 
-		Mat imgGauss = new Mat();				      
-		Imgproc.GaussianBlur(img2, imgGauss, new Size(5,5),3);			    
-		Imgproc.adaptiveThreshold(imgGauss, imgGauss, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,7,5);
-		mostraImg(imgGauss,"Gauss");
-		
-		Mat imgblur = new Mat();
-		Imgproc.blur(img2, imgblur, new Size(5,5));
-		Imgproc.adaptiveThreshold(imgblur, imgblur, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,7,7);
-		Imgproc.adaptiveThreshold(imgblur, imgblur, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV,5,5);
-		
-		mostraImg(imgblur,"Blur");
-
-		/*Filtro da Mediana: Eficiente para o propósito de filtrar imagens, pois ele reduz a quantidade de variação de
-	      intensidade entre um pixel e seus vizinhos, eliminando ruídos.*/
-
-		Mat imgMediana = new Mat();
-		Imgproc.blur(img2, imgMediana, new Size(5,5));
-		Imgproc.medianBlur(imgMediana, imgMediana,5);			     	      
-		Imgproc.adaptiveThreshold(imgMediana, imgMediana, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,7,7);
-		//Imgproc.adaptiveThreshold(imgMediana, imgMediana, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV,5,5);				      
-		Imgproc.resize(imgMediana,imgMediana,new Size(118,118));
-		
-		Imgcodecs.imwrite("placa2.png",imgGauss );
-		
-		mostraImg(imgMediana,"Mediana");
-
-		Mat suavCombinada = new Mat();
-		Imgproc.medianBlur(img2, suavCombinada,7);
-		Imgproc.GaussianBlur(suavCombinada, suavCombinada, new Size(3,3),5);
-		Imgproc.medianBlur(suavCombinada, suavCombinada,7);			     
-		Imgproc.adaptiveThreshold(suavCombinada, suavCombinada, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,7,7);
-		Imgproc.adaptiveThreshold(suavCombinada, suavCombinada, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY_INV,5,5);	
-
-	mostraImg(suavCombinada,"Gaussian + Mediana");
-
-	HighGui.waitKey();	// sempre será a ultima linha do codico para mostra as janelas...
-		/*
-		try {
-			Process aaa = new ProcessBuilder("C:\\Program Files (x86)\\Tesseract-OCR\\tesseract.exe "+
-					" C:\\Users\\Felipe\\Documents\\GitHub\\workspace\\ReconhecimentoDeImagem\\src\\placa2.jpg  C:\\Users\\Felipe\\Pictures\\out").start();
-			
-			String texto = lerArquivo("C:\\Users\\Felipe\\Pictures\\out");
-			
-			System.out.println(texto);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}	*/
-	}      
+		HighGui.waitKey();
+	}
 
 	private static String lerArquivo(String caminhoDoArquivo) throws IOException {
 		String textoconvertido="";
-		
+
 		BufferedReader leitor = null;
-		
+
 		try {
-					
+
 			leitor = new BufferedReader(new FileReader(caminhoDoArquivo));
-			caminhoDoArquivo = leitor.readLine();
-			leitor.close();
+			 while(leitor.ready()){
+				 textoconvertido += leitor.readLine() + "\n";
+             }			
 			
+			leitor.close();
+
 		} catch (Exception e) {
 			// TODO: handle exception
-			
+
 		}
-		leitor.close();
+		//leitor.close();
 		return textoconvertido;
 	}
 
 	public static void mostraImg(Mat img, String titulo){			
 
 		HighGui.namedWindow(titulo, HighGui.WINDOW_AUTOSIZE);
-		HighGui.resizeWindow(titulo, 500, 500); 				// **pode definir a altura e a largura da janela, não redimenciona a imagem.**
-		HighGui.moveWindow(titulo, eixoX, 0);	 				// posição da janela en relação a tela segundo os valores de x e y
-		eixoX=eixoX+250; 										// altera o valor segundo as suas preferencias
+		HighGui.resizeWindow(titulo, 500, 500); 				
+		HighGui.moveWindow(titulo, eixoX, 0);	 				
+		eixoX=eixoX+250; 										
 		HighGui.imshow(titulo, img);	
-		
+
 	}		
-
-
-
-
 }
