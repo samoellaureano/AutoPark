d9626078a3ff5761b37f07dc9c71e6e88a8057c9
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-11-01 14:33:14 -0300

    Reconhecimento de imagem de pytom para java

--- ReconhecimentoDeImagem/src/br/com/reconhecimentoImagem/main/Decomposicao.java
+++ ReconhecimentoDeImagem/src/br/com/reconhecimentoImagem/main/Decomposicao.java
@@ -9,12 +9,18 @@ import java.util.List;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.video.Video;
+import org.opencv.videoio.VideoCapture;
+
 import net.sourceforge.tess4j.ITesseract;
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
@@ -28,77 +34,14 @@ public class Decomposicao {
 
 
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-
-		String localImg1="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa1.jpg";
-		String localImg2="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa2.jpg";
-		String localImg3="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa3.jpg";
-		String localImg4="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa4.jpg";
-		String localImg5="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa5.jpg";
-		String localImg6="C:\\Users\\Felipe\\Documents\\GitHub\\TCC\\ReconhecimentoDeImagem\\src\\placa6.png";
-
-		Mat img = Imgcodecs.imread(localImg2);
 	
-		List<MatOfPoint> contourList = new ArrayList<MatOfPoint>(); //A list to store all the contours
-		Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2GRAY, 90);
-		Imgproc.boxFilter(img, img, Imgproc.ADAPTIVE_THRESH_MEAN_C, new Size(1,3));	
-		Imgproc.equalizeHist(img, img);	
+		String source = "resource/video1-480p.mp4";
+		String sourceHD = "resource/video1-720p.mkv";
+		String sourceImg = "C:/Tesseract-OCR/saidas/roi-ocr.jpg";
+		buscaRetanguloPlaca(sourceHD);
 		
-		Imgproc.medianBlur(img, img,5);        
-		Imgproc.blur(img, img,new Size(1,3));
-	
 		
-		Mat structImage = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
-		Imgproc.erode(img, img, structImage);
 		
-		Mat structImage2 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,1));
-		Imgproc.dilate(img, img, structImage2);
-		
-	//	Imgproc.Canny(img, img, 90, 100);
-		
-		//	Imgproc.cvtColor(img, img, Imgproc.THRESH_OTSU);
-		//	Imgproc.Canny(img, cannyEdges, Imgproc.THRESH_OTSU, Imgproc.ADAPTIVE_THRESH_MEAN_C);
-		//	Imgproc.medianBlur(cannyEdges, cannyEdges,1);    
-		//	Imgproc.adaptiveThreshold(cannyEdges, cannyEdges, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,3,3);
-		//Converting the image to grayscale
-		//   Imgproc.cvtColor(img, grayMat, Imgproc.COLOR_BGR2GRAY);
-		//   Imgproc.Canny(img, cannyEdges, 90, 100);
-		//finding contours
-
-
-		/*		
-	   	Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2GRAY, 50);
-	   	Imgproc.equalizeHist(img, img);	
-	    Imgproc.medianBlur(img, img,5);        
-		Imgproc.blur(img, img,new Size(1,3));
-
-		Mat structImage = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
-		Imgproc.erode(img, img, structImage);
-		Mat structImage2 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(2,1));
-		Imgproc.dilate(img, img, structImage2);
-		Imgproc.cvtColor(img, img, Imgproc.THRESH_OTSU);
-		Imgproc.Canny(img, img, Imgproc.THRESH_OTSU, Imgproc.ADAPTIVE_THRESH_MEAN_C);
-
-
-	  	Imgproc.boxFilter(img, img, Imgproc.ADAPTIVE_THRESH_MEAN_C, new Size(2,2));		
-	  	Imgproc.Canny(img, img, Imgproc.THRESH_OTSU, Imgproc.ADAPTIVE_THRESH_MEAN_C);
-	  	Imgproc.resize(img,img,new Size(300,300));
-
-	  Imgproc.GaussianBlur(img, img, new Size(0,0),0.5);		
-	  Imgproc.GaussianBlur(img, img, new Size(1,1),1);
-	  Core.addWeighted(img, 1.37, img, -0.75, 0, img);		
-	  Imgproc.threshold(img,img,100,255,1);
-	  Imgproc.adaptiveThreshold(img, img, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,3,3);
-
-	 Imgproc.adaptiveThreshold(img, img, 255,Imgproc.ADAPTIVE_THRESH_MEAN_C, Imgproc.THRESH_BINARY,3,3);	
-	 Imgproc.GaussianBlur(img, img, new Size(1,1),1);
-	 Imgproc.erode(img, img, structImage2); 
-
-		Mat hierarchy = new Mat();
-		Mat image = img.clone();
-		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Imgproc.findContours(img, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);  */
-
-		Imgcodecs.imwrite("placa2.png",img);		
 
 		mostraImg(img, "Imagem");
 
@@ -115,6 +58,14 @@ public class Decomposicao {
 
 		HighGui.waitKey();
 
+	}
+
+	private static void buscaRetanguloPlaca(String sourceHD) {
+	
+		
+		
+		
+		
 	}
 
 	private static String lerArquivo(String caminhoDoArquivo) throws IOException {
@@ -143,7 +94,45 @@ public class Decomposicao {
 		HighGui.resizeWindow(titulo, 500, 500); 				
 		HighGui.moveWindow(titulo, eixoX, 0);	 				
 		eixoX=eixoX+250; 										
-		HighGui.imshow(titulo, img);	
+		HighGui.imshow(titulo, img);
+    }
+	
+	
+	
 
-	}		
+	
+	@SuppressWarnings("unchecked") // verificar a forma defazer para recortar a imagem grava
+	public Mat localizaPlaca(List<MatOfPoint2f>contornos,Mat img){
+		double perimetro=0;
+		MatOfPoint2f aprox =null;
+		Mat imgRoi = new Mat();
+		for (MatOfPoint2f matOfPoint : contornos){
+			
+			aprox=new MatOfPoint2f();
+			perimetro = Imgproc.arcLength(matOfPoint,true);
+			
+			if(perimetro>200 && perimetro<600) {
+				
+				Imgproc.approxPolyDP(matOfPoint,aprox, 0.03*perimetro, true);
+				
+				if(aprox.total()==4){
+					
+					Imgproc.drawContours(img, (List<MatOfPoint>) matOfPoint, -1, new Scalar(0, 255,0),2);
+					Rect pontos = Imgproc.boundingRect(matOfPoint);
+					Imgproc.rectangle(img,pontos.br(),pontos.tl(), new Scalar(0, 255,0),2);					
+					imgRoi=matOfPoint;
+					break;
+				}
+			}		
+		}
+		
+		Imgcodecs.imwrite("placa2.png", imgRoi);
+		return imgRoi; 
+	}
+	
+	
+	
+	
+	
+	
 }
