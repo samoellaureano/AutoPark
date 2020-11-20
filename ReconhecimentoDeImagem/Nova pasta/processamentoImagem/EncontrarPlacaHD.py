6d0c4afc3c6ec6aecb791dbd2f12b8d186e97758
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-11-19 23:09:13 -0300

    mais uma alteração em python, amanhã talvez de boa

--- ReconhecimentoDeImagem/Nova pasta/processamentoImagem/EncontrarPlacaHD.py
+++ ReconhecimentoDeImagem/Nova pasta/processamentoImagem/EncontrarPlacaHD.py
@@ -4,63 +4,42 @@ import pytesseract
 import cv2
 
 
-
-def findPlace(contornos, imagem):  #
-    print("Captura de contornos")
+def findPlace(contornos, imagem):
     for c in contornos:
-        # perimetro do contorno, verifica se o contorno é fechado
+
         perimetro = cv2.arcLength(c, True)
-        if perimetro > 918 and perimetro < 1350:
-            # aproxima os contornos da forma correspondente
-            approx = cv2.approxPolyDP(c, 0.03 * perimetro, True)
-            # verifica se é um quadrado ou retangulo de acordo com a qtd de vertices
+        if perimetro > 900 and perimetro < 1250:
+            approx = cv2.approxPolyDP(c, 0.05 * perimetro, True)
             if len(approx) == 4:
-                # Contorna a placa atraves dos contornos encontrados
-                # cv2.drawContours(imagem, [c], -1, (0, 255, 0), 2)
+
                 (x, y, lar, alt) = cv2.boundingRect(c)
-                cv2.rectangle(imagem, (x, y), (x + lar, y + alt), (0, 255, 0), 2)
-                # segmenta a placa da imagem
-                roi = imagem[(y + 15):y + alt, x:x + lar]
-                # salva a imagem segmentada em "C:/Tesseract-OCR/saidas/"
-                cv2.imwrite("C:/Tesseract-OCR/saidas/placa.jpg", roi)
+                if alt < lar:
+                    cv2.rectangle(imagem, (x, y), (x + lar, y + alt), (0, 255, 0), 3)
+                    roi = imagem[y:y + alt, x:x + lar]
+                    cv2.imwrite("C:/Tesseract-OCR/saidas/placa.jpg", roi)
 
     return imagem
 
 
 def reconhecimentoOCR(path_img):
-
     print("reconhecimento da placa")
-    # Ler a entrada da imagem e salva na variavel entrada
+
     img = cv2.imread(path_img)
     cv2.imshow("ENTRADA", img)
 
-    # aumenta a resoluca da imagem da placa em 4x
-    #img = cv2.resize(entrada, None, fx=4, fy=4, interpolation=cv2.INTER_CUBIC)
-
-    # Converte para escala de cinza
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Escala Cinza", img)
 
-    # Binariza a imagem (preto e branco)
-    ret, img = cv2.threshold(img, 70, 255, cv2.THRESH_OTSU)
+    ret, img = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
     cv2.imshow("Limiar", img)
 
-    # Aplica um desfoque na Imagem
     img = cv2.GaussianBlur(img, (5, 5), 0)
     cv2.imshow("Desfoque", img)
-    #kernel = np.ones((5, 5), np.uint8)
-    #img = cv2.erode(img,kernel,1,1,cv2.BORDER_REFLECT)
 
-    # Grava o resultado no endereco path+/ocr.jpg
     cv2.imwrite(path_img + "-ocr.jpg", img)
-
-    # Abre a imagem gravada para ser feita o reconhecimento OCR
-    # e salva o resultado na variavel saida
     imagem = Image.open(path_img + "-ocr.jpg")
     saida = pytesseract.image_to_string(imagem, lang='eng')
 
-    # Realiza um filtro nos caracteres obtidos 
-    # eliminando possiveis ruidos reconhecidos
     if len(saida) > 0:
         print(saida)
         texto = removerChars(saida)
@@ -72,46 +51,46 @@ def reconhecimentoOCR(path_img):
     tkinter.Label(janela, text=texto, font=("Helvetica", 50)).pack()
     janela.mainloop()
 
+
 def removerChars(text):
-    str = "'‘!@#%¨&*()_+:;><^^}{`?|~¬/=,.'ºª»'\n \s+   "
+    str = "'‘!@#%¨&*()_+:;><^^}{`?|~¬/=,.'ºª»'\n \s+    °"
     for x in str:
         text = text.replace(x, '')
+    text = text.replace(str, '')
+
     return text
 
-# Captura ou Video
-video = cv2.VideoCapture('resource\\placa21.mp4')
+
+video = cv2.VideoCapture('resource\\placa7.mp4')
 
 while (video.isOpened()):
     print("Video rodando")
     ret, frame = video.read()
 
-    if (ret == False):
+    if ret == False:
         break
 
-    # area de localização
-    area = frame[500:, 450:1250]
-    # escala de cinza
-    img_result = cv2.cvtColor(area, cv2.COLOR_BGR2GRAY)
+    area = frame[400:, 225:1250]
+    # area = frame[300:, 150:1255]
 
-    # desfoque
+    img_result = cv2.cvtColor(area, cv2.COLOR_BGR2GRAY)
     img_result = cv2.GaussianBlur(img_result, (5, 5), 0)
 
-    # limiarização
     ret, img_result = cv2.threshold(img_result, 90, 255, cv2.THRESH_BINARY)
-
-    # lista os contornos
+    img_result = cv2.Canny(img_result, 100, 200)
     img, contornos, hier = cv2.findContours(img_result, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
     # limite horizontal
-    #cv2.line(frame, (0, 500), (1280, 500), (0, 0, 255), 1)
+    cv2.line(frame, (0, 500), (1880, 500), (0, 0, 255), 3)
     # limite vertical 1
-    #cv2.line(frame, (300, 0), (300, 720), (0, 0, 255), 1)
+    cv2.line(frame, (300, 0), (300, 1200), (0, 0, 255), 3)
     # limite vertical 2
-    #cv2.line(frame, (800, 0), (800, 720), (0, 0, 255), 1)
+    cv2.line(frame, (1200, 0), (1200, 1200), (0, 0, 255), 3)
 
     cv2.imshow('FRAME', frame)
     findPlace(contornos, area)
     cv2.imshow('RES', area)
+    cv2.imshow("Proc", img_result)
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
