57ccc5bacf5a36f5a161cadc28438b2c5b323e89
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-12-03 23:24:43 -0300

    Final teste 2

--- resoures/ReconhecimentoDeImagem/Reconhecimento.py
+++ resoures/ReconhecimentoDeImagem/Reconhecimento.py
@@ -9,14 +9,14 @@ import requests
 def localizarPlaca(contornos, imagem):
     for c in contornos:
         perimetro = cv2.arcLength(c, True)
-        if perimetro > 900 and perimetro < 1250:
+        if 600 < perimetro < 870:
             approx = cv2.approxPolyDP(c, 0.05 * perimetro, True)
             if len(approx) == 4:
 
                 (x, y, lar, alt) = cv2.boundingRect(c)
                 if alt < lar:
-                    cv2.rectangle(imagem, (x, y), (x + lar, y + alt), (0, 255, 0), 3)
-                    roi = imagem[y:y + alt, x:x + lar]
+                    cv2.rectangle(imagem, (x, y), (x + lar, y + alt), (0, 255, 0), 1)
+                    roi = imagem[y:y + alt+20, x:x + lar]
                     cv2.imwrite("C:/Tesseract-OCR/saidas/placa.jpg", roi)
 
     return imagem
@@ -31,7 +31,7 @@ def reconhecimentoImagem(path_img):
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Escala Cinza", img)
 
-    ret, img = cv2.threshold(img, 90, 120, cv2.CHAIN_APPROX_NONE)
+    ret, img = cv2.threshold(img, 80, 255, cv2.FILE_NODE_NONE)
     cv2.imshow("Limiar", img)
 
     img = cv2.GaussianBlur(img, (5, 5), 0)
@@ -51,17 +51,17 @@ def reconhecimentoImagem(path_img):
     print(texto)
     id = "1"
 
-    response = requests.post("http://localhost:8080/Estacionamento_Automatizado/rest/checkinRest/addCheckin/" + texto + "&" + id)
+    #response = requests.post("http://localhost:8080/Estacionamento_Automatizado/rest/checkinRest/addCheckin/" + texto + "&" + id)
     #response = requests.post("http://localhost:8080/Estacionamento_Automatizado/rest/checkoutRest/addCheckout/" + texto + "&" + id)
 
-    print(response.status_code)
+    #print(response.status_code)
 
     tkinter.Label(janela, text=texto, font=("Helvetica", 50)).pack()
     janela.mainloop()
 
 
 def removerChars(text):
-    str = "'‘-!@#%¨&*()_+:;><^^}{`?|~¬/=,.-'ºª»'\n \s+    °"
+    str = "'[‘-]!@#%¨&*()_+:;><^^}{`?|~¬/=,.-'ºª»'\n \s+    °——————eagonmmmnt"
     for x in str:
         text = text.replace(x, '')
     text = text.replace(str, '')
@@ -69,7 +69,7 @@ def removerChars(text):
     return text
 
 
-video = cv2.VideoCapture('resource\\placa7.mp4')
+video = cv2.VideoCapture('resource\\ANB5022.mp4')
 
 while (video.isOpened()):
     print("Video rodando")
@@ -78,22 +78,22 @@ while (video.isOpened()):
     if ret == False:
         break
 
-    area = frame[400:, 225:1250]
+    area = frame[300:, 200:900]
     result = cv2.cvtColor(area, cv2.COLOR_BGR2GRAY)
     result = cv2.GaussianBlur(result, (5, 5), 0)
-
-    ret, result = cv2.threshold(result, 90, 255, cv2.THRESH_BINARY)
+    ret, result = cv2.threshold(result, 80, 255, cv2.THRESH_BINARY)
 
     img, contornos, hier = cv2.findContours(result, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    cv2.line(frame, (0, 500), (1880, 500), (0, 0, 255), 3)
-    cv2.line(frame, (300, 0), (300, 1200), (0, 0, 255), 3)
-    cv2.line(frame, (1200, 0), (1200, 1200), (0, 0, 255), 3)
+    cv2.line(frame, (0, 200), (1880, 200), (0, 0, 255), 1)
+    cv2.line(frame, (200, 0), (200, 1200), (0, 0, 255), 1)
+    cv2.line(frame, (900, 0), (900, 1200), (0, 0, 255), 1)
 
     cv2.imshow('FRAME', frame)
-    localizarPlaca(contornos, area)
     cv2.imshow('RES', area)
-    cv2.imshow("Proc", result)
+    #cv2.imshow("Proc", result)
+    localizarPlaca(contornos, area)
+
 
     if cv2.waitKey(1) & 0xff == ord('q'):
         break
