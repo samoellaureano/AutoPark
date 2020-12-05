254f789eebf7cc5b8e01976b9b09e43cfd46fe23
Sysm94 <65374666+Sysm94@users.noreply.github.com>
2020-12-05 20:24:58 -0300

    relatório esta sendo gerado em sua forma bruta

--- Estacionamento_Automatizado/WebContent/resources/admin/js/relatorio.js
+++ Estacionamento_Automatizado/WebContent/resources/admin/js/relatorio.js
@@ -1,8 +1,9 @@
-var listaDeRegistros = null;
+
+
 $(document).ready(function () {
     $("#menu").load("menu.html");
     $('#buscarRelatorio').click(function (e) {
-        listaDeRegistros = null;
+       
         var dataInicial = reformatDate($("#buscarDataInicial").val());
         var dataFinal = reformatDate($("#buscarDataFinal").val());
         var cfg = {
@@ -18,10 +19,12 @@ $(document).ready(function () {
         autoPark.ajax.post(cfg);
     });
 
-    visualizarRegistro = function (listaDeRegistros) {
+    visualizarRegistro = function (listaDeRegistros){
+       
         if (listaDeRegistros != undefined) {
             var registroHtml = "";
             if (listaDeRegistros.checkin.length > 0) {
+                geraTabelaPDF(listaDeRegistros);
                 for (var i = 0; i < listaDeRegistros.checkin.length; i++) {
                     if (listaDeRegistros.checkout[i] != undefined) {
                         var data = new Date(listaDeRegistros.checkin[i].dataHora);
@@ -36,7 +39,8 @@ $(document).ready(function () {
                             + "<li>Tempo de Uso:<span>" + (diferenca.getUTCDate() - 1) + "d " + diferenca.getUTCHours() + "h " + diferenca.getUTCMinutes() + "m</span></li>"
                             + "<li>Valor:<span>" + listaDeRegistros.checkout[i].valor + "</span></li></ul>";
                     }
-                };
+                };                
+                
             } else {
                 registroHtml = "<span>Nenhum registro encontrado</span>";
             };
@@ -47,13 +51,42 @@ $(document).ready(function () {
     reformatDate = function (dateStr) {
         dArr = dateStr.split("-");  // ex input "2010-01-18"
         return dArr[2] + "-" + dArr[1] + "-" + dArr[0]; //ex out: "18/01/10"
-    }
+    };
+
+
+    geraTabelaPDF = function(listaDeRegistros){
+    	
+        var html="<table id='idtableEdit2'><thead>"
+				+"<th scope='col'>Veiculo</th><th scope='col'>tempo de Uso</th><th scope='col'>Valor</th>"
+				+"</tr></thead><tbody>";		
+
+                for(var i=0; i<listaDeRegistros.checkin.length; i++){	
+                    if (listaDeRegistros.checkout[i] != undefined) {
+                    var data = new Date(listaDeRegistros.checkin[i].dataHora);
+
+                        const now = new Date(listaDeRegistros.checkout[i].dataHora); // Data de hoje
+                        const past = new Date(listaDeRegistros.checkin[i].dataHora); // Outra data no passado
+                        const diff = Math.abs(now.getTime() - past.getTime());
+                        var diferenca = new Date(diff);								
+                    
+                    html+="<tr><td>"+ listaDeRegistros.checkin[i].veiculo.placa +"</td>"
+                            +"<td>"+ (diferenca.getUTCDate() - 1) + "d " + diferenca.getUTCHours() + "h " + diferenca.getUTCMinutes() + "m</td>"
+                            +"<td>"+ listaDeRegistros.checkout[i].valor + "</td></tr>";						
+                    };
+                };
+                
+                html+="</tbody></table>";	
+                $('#gridRelatorio').html(html);
+    };
 
     gerarPDF = function (){
+    	
         //trazer  importe para a o html e referenciar a tabela
         //sem css de prerencia e apontar a tabela para o doc.autotable com o id da mesma
+       
         var doc = new jsPDF();
         doc.setFontSize(11);
+        
         doc.text(10, 10, 'Logos Assistência Tecnica Especializada');
         doc.text(10, 15, 'Rua: Dr. João Colin, nº 780, joinville - SC ');
         doc.text(10, 20, 'Telefone: (47) 3227 9076');
@@ -61,14 +94,14 @@ $(document).ready(function () {
         doc.setFontSize(22);
         doc.text(65, 45, 'Relatório da Empresa');
         doc.setFontSize(11);
-        doc.text(20, 65, 'Status das ordem de Serviço: ' + status);
-        doc.text(20, 70, 'Valor Total: ' + vlt);
+        doc.text(20, 65, 'Status das ordem de Serviço: ');
+        doc.text(20, 70, 'Valor Total: ');
 
         doc.autoTable({ html: "#idtableEdit2", startY: 95 });
-
         var data = new Date();
-        var mes = parseInt(data.getMonth()) + parseInt(1);
-        var dia = data.getDate() + "_" + mes + "_" + data.getFullYear();
-        doc.save("Relatorio - " + dia + ".pdf");
-    }
+        var mes = parseInt(data.getMonth())+parseInt(1);
+        var dia = data.getDate()+"_"+ mes+"_"+ data.getFullYear();
+        doc.save("Relatorio - "+dia+".pdf");
+        
+    };
 });
