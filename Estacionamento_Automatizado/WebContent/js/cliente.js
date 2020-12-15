065673ee327d91a13b24a41a6d07f701afa731b2
eduardohmussi <69681089+eduardohmussi@users.noreply.github.com>
2020-12-14 21:32:48 -0300

    Validação CPF/CNPJ
    Criada validação do CPF/CNPJ no cadastro do cliente.

--- Estacionamento_Automatizado/WebContent/js/cliente.js
+++ Estacionamento_Automatizado/WebContent/js/cliente.js
@@ -1,17 +1,20 @@
 $( document ).ready(function() {
-	$("#cpf").mask('000.000.000-00');
-var cliente = new Object();
-var usuario = new Object();
+	
+	
+	
+	var cliente = new Object();
+	var usuario = new Object();
 
 	$("#cadCliente").click(function(e){
+		
 	        cliente.nome    = $("#nome").val();
-					usuario.cpf     = $("#cpf").val();
-					usuario.cpf = usuario.cpf.replace(/\./g, "");
-					usuario.cpf = usuario.cpf.replace(/\-/g, "");					
-					cliente.celular = $("#celular").val();
-					cliente.celular = cliente.celular.replace(/[^0-9]/g, '');
+			usuario.cpf     = $("#cpf").val();
+			usuario.cpf     = usuario.cpf.replace(/\./g, "");
+			usuario.cpf     = usuario.cpf.replace(/\-/g, "");					
+			cliente.celular = $("#celular").val();
+			cliente.celular = cliente.celular.replace(/[^0-9]/g, '');
 	        cliente.email   = $("#email").val();
-					usuario.senha 	= btoa($("#senha").val());
+			usuario.senha 	= btoa($("#senha").val());
 
 			cliente.usuario = usuario;
 	   
@@ -40,5 +43,141 @@ var usuario = new Object();
 	        };
 	        autoPark.ajax.post(cfg);
 	    });// fim da fução
+
+	validaCpfCnpj = function(val) {			   
+		
+		if( val.length > 0 ) {
+			if (val.length == 14) {
+		        var cpf = val.trim();
+		     
+		        cpf = cpf.replace(/\./g, '');
+		        cpf = cpf.replace('-', '');
+		        cpf = cpf.split('');
+		        
+		        var v1 = 0;
+		        var v2 = 0;
+		        var aux = false;
+		        
+		        for (var i = 1; cpf.length > i; i++) {
+		            if (cpf[i - 1] != cpf[i]) {
+		                aux = true;   
+		            }
+		        } 
+		        
+		        if (aux == false) {
+					alert('CPF INVALIDO!');
+					$("#cpf").focus();
+		            return false; 
+		        } 
+		        
+		        for (var i = 0, p = 10; (cpf.length - 2) > i; i++, p--) {
+		            v1 += cpf[i] * p; 
+		        } 
+		        
+		        v1 = ((v1 * 10) % 11);
+		        
+		        if (v1 == 10) {
+		            v1 = 0; 
+		        }
+		        
+		        if (v1 != cpf[9]) {
+					alert('CPF INVALIDO!');
+					$("#cpf").focus();
+		            return false; 
+		        } 
+		        
+		        for (var i = 0, p = 11; (cpf.length - 1) > i; i++, p--) {
+		            v2 += cpf[i] * p; 
+		        } 
+		        
+		        v2 = ((v2 * 10) % 11);
+		        
+		        if (v2 == 10) {
+		            v2 = 0; 
+		        }
+		        
+		        if (v2 != cpf[10]) {
+					alert('CPF INVALIDO!');
+					$("#cpf").focus();
+		            return false; 
+		        } else {   
+		            return true; 
+		        }
+		    } else if (val.length == 18) {
+		        var cnpj = val.trim();
+		        
+		        cnpj = cnpj.replace(/\./g, '');
+		        cnpj = cnpj.replace('-', '');
+		        cnpj = cnpj.replace('/', ''); 
+		        cnpj = cnpj.split(''); 
+		        
+		        var v1 = 0;
+		        var v2 = 0;
+		        var aux = false;
+		        
+		        for (var i = 1; cnpj.length > i; i++) { 
+		            if (cnpj[i - 1] != cnpj[i]) {  
+		                aux = true;   
+		            } 
+		        } 
+		        
+		        if (aux == false) {
+					$("#cpf").focus();  
+		            return false; 
+		        }
+		        
+		        for (var i = 0, p1 = 5, p2 = 13; (cnpj.length - 2) > i; i++, p1--, p2--) {
+		            if (p1 >= 2) {  
+		                v1 += cnpj[i] * p1;  
+		            } else {  
+		                v1 += cnpj[i] * p2;  
+		            } 
+		        } 
+		        
+		        v1 = (v1 % 11);
+		        
+		        if (v1 < 2) { 
+		            v1 = 0; 
+		        } else { 
+		            v1 = (11 - v1); 
+		        } 
+		        
+		        if (v1 != cnpj[12]) { 
+					alert('CNPJ INVALIDO!');
+					$("#cpf").focus();
+		            return false; 
+		        } 
+		        
+		        for (var i = 0, p1 = 6, p2 = 14; (cnpj.length - 1) > i; i++, p1--, p2--) { 
+		            if (p1 >= 2) {  
+		                v2 += cnpj[i] * p1;  
+		            } else {   
+		                v2 += cnpj[i] * p2; 
+		            } 
+		        }
+		        
+		        v2 = (v2 % 11); 
+		        
+		        if (v2 < 2) {  
+		            v2 = 0;
+		        } else { 
+		            v2 = (11 - v2); 
+		        } 
+		        
+		        if (v2 != cnpj[13]) {
+					alert('CNPJ INVALIDO!');
+					$("#cpf").focus();
+		            return false; 
+		        } else {
+		            return true; 
+		        }
+		    } else {
+				alert('CPF/CNPJ INVALIDO!');
+				$("#cpf").focus();
+		        return false;
+		    }
+		
+		}
+	 };
 
 });
